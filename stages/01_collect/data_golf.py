c1c990f44263e7b93a5ce546e40a6a185229cd3a
Joseph Urciuoli <joe@courted.io>
2022-06-05 10:05:21 -0400

    adding historical betting

---
+++ stages/01_collect/data_golf.py
@@ -0,0 +1,117 @@
+import requests
+import pandas as pd
+
+BOOKS = [
+    "bet365",
+    "betcris",
+    "betmgm",
+    "betonline",
+    "betway",
+    "bovada",
+    "corale",
+    "circa",
+    "draftkings",
+    "fanduel",
+    "pinnacle",
+    "skybet",
+    "sportsbook",
+    "unibet",
+    "williamhill",
+]
+
+MARKETS = ["win", "top_5", "top_10", "top_20", "make_cut", "mc"]
+
+
+def _get_events_list() -> list:
+    """Gets the events from Data Golf."""
+    url = "https://feeds.datagolf.com/historical-odds/event-list?tour=pga&key=9e89123f99ee57b21131000936f1"
+    response = requests.get(url)
+    events = response.json()
+    return events
+
+
+def pre_tournament_predictions_archive():
+    """
+    Collects the data from the pre-tournament predictions archive on data golf.
+    """
+    base_url = "https://feeds.datagolf.com/preds/pre-tournament?event_id=$$EVENT$$&year=$$YEAR$$&odds_format=percent&key=9e89123f99ee57b21131000936f1"
+    df = pd.DataFrame()
+    events = _get_events_list()
+    for event in events:
+        print("Event Name: ", event.get("event_id"))
+        print("Event year: ", event.get("calendar_year"))
+        if event.get("archived_preds") == "no":
+            continue
+        event_url = base_url.replace(
+            "$$YEAR$$", str(event.get("calendar_year"))
+        ).replace("$$EVENT$$", str(event.get("event_id")))
+        response = requests.get(event_url)
+        event_df = pd.DataFrame(response.json()["baseline"])
+        event_df["event_id"] = event.get("event_id")
+        event_df["year"] = event.get("calendar_year")
+        event_df["event_name"] = event.get("event_name")
+        if not df.empty:
+            df = pd.concat([df, event_df])
+        else:
+            df = event_df
+        df.to_csv("../data/data_golf_pre_tournament_predictions_archive.csv")
+
+
+def historical_outrights():
+    """Collects the data from the Historical Outrights on data golf."""
+    events = _get_events_list()
+    base_url = "https://feeds.datagolf.com/historical-odds/outrights?tour=pga&event_id=$$EVENT$$&year=$$YEAR$$&market=$$MARKET$$&book=$$BOOK$$&odds_format=percent&key=9e89123f99ee57b21131000936f1"
+    all_event_df = pd.DataFrame()
+    for event in events:
+        print("Event Name: ", event.get("event_id"))
+        print("Event Year: ", event.get("calendar_year"))
+        event_url = base_url.replace("$$EVENT$$", str(event.get("event_id"))).replace(
+            "$$YEAR$$", str(event.get("calendar_year"))
+        )
+        event_df = pd.DataFrame()
+        for market in MARKETS:
+            for book in BOOKS:
+                event_market_book_url = event_url.replace("$$MARKET$$", market).replace(
+                    "$$BOOK$$", book
+                )
+                try:
+                    response = requests.get(event_market_book_url)
+                    data = response.json()
+                except Exception as e:
+                    print(e)
+                    continue
+                odds = data.get("odds")
+                if not isinstance(odds, list):
+                    continue
+                event_market_book_df = pd.DataFrame(odds)
+                event_market_book_df = event_market_book_df[
+                    ["open_odds", "close_odds", "dg_id"]
+                ]
+                event_market_book_df.rename(
+                    columns={
+                        "open_odds": f"{book}_{market}_open_odds",
+                        "close_odds": f"{book}_{market}_close_odds",
+                    },
+                    inplace=True,
+                )
+                if event_df.empty:
+                    event_market_book_df["event_id"] = data.get("event_id")
+                    event_market_book_df["event_name"] = data.get("event_name")
+                    event_df = event_market_book_df.copy()
+                else:
+                    event_df = pd.merge(
+                        left=event_df,
+                        right=event_market_book_df,
+                        on="dg_id",
+                        how="outer",
+                    )
+        if not all_event_df.empty:
+            all_event_df = pd.concat([all_event_df, event_df])
+        else:
+            all_event_df = event_df
+        print("All Event DF", all_event_df.shape)
+    all_event_df.to_csv("../data/data_golf_historical_betting.csv")
+
+
+pre_tournament_predictions_archive()
+historical_outrights()
