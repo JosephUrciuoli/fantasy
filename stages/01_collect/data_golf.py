19a1057ae6a940e3304a6b3eb6b72c23d1aedde7
Joseph Urciuoli <joe@courted.io>
2022-06-05 11:23:18 -0400

    adding historical dfs data

--- stages/01_collect/data_golf.py
+++ stages/01_collect/data_golf.py
@@ -21,10 +21,20 @@ BOOKS = [
 
 MARKETS = ["win", "top_5", "top_10", "top_20", "make_cut", "mc"]
 
+SITES = ["draftkings", "fanduel"]
+
 
 def _get_events_list() -> list:
     """Gets the events from Data Golf."""
     url = "https://feeds.datagolf.com/historical-odds/event-list?tour=pga&key=9e89123f99ee57b21131000936f1"
+    response = requests.get(url)
+    events = response.json()
+    return events
+
+
+def _get_dfs_events_list() -> list:
+    """Gets the events from Data Golf DFS endpoint."""
+    url = "https://feeds.datagolf.com/historical-dfs-data/event-list?&key=9e89123f99ee57b21131000936f1"
     response = requests.get(url)
     events = response.json()
     return events
@@ -54,7 +64,7 @@ def pre_tournament_predictions_archive():
             df = pd.concat([df, event_df])
         else:
             df = event_df
-        df.to_csv("../data/data_golf_pre_tournament_predictions_archive.csv")
+        df.to_csv("../../data/data_golf_pre_tournament_predictions_archive.csv")
 
 
 def historical_outrights():
@@ -110,8 +120,74 @@ def historical_outrights():
         else:
             all_event_df = event_df
         print("All Event DF", all_event_df.shape)
-    all_event_df.to_csv("../data/data_golf_historical_betting.csv")
+    all_event_df.to_csv("../../data/data_golf_historical_betting.csv")
+
+
+def historical_dfs_data():
+    """Collects the data from the Historical Outrights on data golf."""
+    events = _get_events_list()
+    base_url = "https://feeds.datagolf.com/historical-dfs-data/points?tour=pga&site=$$SITE$$&event_id=$$EVENT$$&year=$$YEAR$$&key=9e89123f99ee57b21131000936f1"
+    all_event_df = pd.DataFrame()
+    for event in events:
+        print("Event Name: ", event.get("event_id"))
+        print("Event Year: ", event.get("calendar_year"))
+        event_url = base_url.replace("$$EVENT$$", str(event.get("event_id"))).replace(
+            "$$YEAR$$", str(event.get("calendar_year"))
+        )
+        event_df = pd.DataFrame()
+        for site in SITES:
+            event_site_url = event_url.replace("$$SITE$$", site)
+            try:
+                response = requests.get(event_site_url)
+                data = response.json()
+            except Exception as e:
+                print(e)
+                continue
+            dfs_output = data.get("dfs_points")
+            if not isinstance(dfs_output, list):
+                continue
+            event_site_df = pd.DataFrame(dfs_output)
+            event_site_df = event_site_df[
+                [
+                    "salary",
+                    "ownership",
+                    "hole_score_pts",
+                    "finish_pts",
+                    "total_pts",
+                    "fin_text",
+                    "dg_id",
+                ]
+            ]
+            event_site_df.rename(
+                columns={
+                    "salary": f"{site}_salary",
+                    "ownership": f"{site}_ownership",
+                    "hole_score_points": f"{site}_hole_score_points",
+                    "finish_points": f"{site}_finish_points",
+                    "total_pts": f"{site}_total_pts",
+                    "fin_text": f"{site}_fin_text",
+                },
+                inplace=True,
+            )
+            if event_df.empty:
+                event_site_df["event_id"] = data.get("event_id")
+                event_site_df["event_name"] = data.get("event_name")
+                event_df = event_site_df.copy()
+            else:
+                event_df = pd.merge(
+                    left=event_df,
+                    right=event_site_df,
+                    on="dg_id",
+                    how="outer",
+                )
+        if not all_event_df.empty:
+            all_event_df = pd.concat([all_event_df, event_df])
+        else:
+            all_event_df = event_df
+        print("All Event DF", all_event_df.shape)
+    all_event_df.to_csv("../../data/data_golf_historical_dfs.csv")
 
 
 pre_tournament_predictions_archive()
 historical_outrights()
+historical_dfs_data()
